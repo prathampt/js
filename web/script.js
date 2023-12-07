15626a7207f8178b1a75fe295c5ef91a563bb52a
prathampt <prathamt12345@gmail.com>
2023-12-07 15:32:29 +0530

    first commit

---
+++ web/script.js
@@ -0,0 +1,13 @@
+// JavaScript
+
+let str = "This is a string";
+// alert("Hello Pratham!!!");
+let num = 19;
+
+console.log("This is just first code.");
+console.log(str);
+console.log(typeof num);
+num = String(num);
+console.log(typeof num);
+
+let name = prompt("What is your name?");
